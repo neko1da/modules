fd6144c298f8644299022c94df89c27da8944506
neko1da <56790473+neko1da@users.noreply.github.com>
2019-10-20 21:00:58 +0900

    Update mTresk-sargo.sh

--- mTresk-sargo.sh
+++ mTresk-sargo.sh
@@ -1,1 +1,22 @@
+#neko1da/usr/bin/env bash
+
+cd
+
+sudo apt install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev libncurses5 git repo openjdk-8-jdk
+
+cd
+
+mkdir mTresk
+
+mkdir -p ~/bin
+
+curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+
+chmod a+x ~/bin/repo
+
+cd /mTresk
+
+repo init -u git://github.com/mtresk/android.git -b android-10.0
+
+repo sync -j8
 
